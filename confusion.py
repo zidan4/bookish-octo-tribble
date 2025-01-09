2bec2f334cf9af51e7aa0fc260d885241de87adc
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-01-09 17:44:59 +0300

    Update confusion.py

--- confusion.py
+++ confusion.py
@@ -10,4 +10,4 @@ confusion_matrix = metrics.confusion_matrix(actual, predicted)
 cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = [0, 1])
 
 cm_display.plot()
-plt.show() 
+plt.show()
